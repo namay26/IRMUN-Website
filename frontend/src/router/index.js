01621d6539dde263437ac3c7df22c6000d7ce160
bipashabg <bipasha_bg@es.iitr.ac.in>
2024-11-25 18:16:14 +0530

    add eb page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/components/Home.vue";
 import CAPortal from "@/components/CAPortal.vue";
+import EBPortal from "@/components/EBPortal.vue";
 
 const routes = [
   { path: "/", name: "Home", component: Home },
   { path: "/ca-portal", name: "CAPortal", component: CAPortal },
+  { path: "/eb-portal", name: "EBPortal", component: EBPortal }
 ];
 
 const router = createRouter({
