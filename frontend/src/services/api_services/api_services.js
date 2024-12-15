be5298f23b8dc5afe23c80b33d250317e59f8bd0
namay26 <namayrohatgi@gmail.com>
2024-12-16 00:27:03 +0530

    Make changes

--- frontend/src/services/api_services/api_services.js
+++ frontend/src/services/api_services/api_services.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosClient = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: "https://irmun.iitr.ac.in",
   headers:{
     "Content-Type": "application/json",
   }
